7d83e3ed7f7540867e8e878b9228291972259aad
artmarkov <artmarkov@mail.ru>
2019-07-21 21:41:52 +0300

    refactor theme

--- views/default/update.php
+++ views/default/update.php
@@ -11,6 +11,10 @@ $this->params['breadcrumbs'][] = ['label' => Comments::t('comments', 'Comments')
 $this->params['breadcrumbs'][] = Yii::t('art', 'Update');
 ?>
 <div class="comment-update">
-    <h3 class="lte-hide-title"><?= Html::encode($this->title) ?></h3>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3 class="page-title"><?=  Html::encode($this->title) ?></h3>            
+        </div>
+    </div>
     <?= $this->render('_form', compact('model')) ?>
 </div>
