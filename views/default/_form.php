8bf5b6447ab88c7f65fad696b2b70e9ca62d516f
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2020-02-10 18:39:07 +0300

    refactor form

--- views/default/_form.php
+++ views/default/_form.php
@@ -18,11 +18,47 @@ use artsoft\widgets\ActiveForm;
     ])
     ?>
 
-    <div class="row">
-        <div class="col-md-8">
+    <div class="panel panel-default">
 
-            <div class="panel panel-default">
-                <div class="panel-body">
+        <div class="panel-heading">
+            <div class="form-group clearfix">
+                <label class="control-label" style="float: left; padding-right: 5px;">
+                    <?= $model->attributeLabels()['username'] ?> :
+                </label>
+                <span><?= $model->author ?></span>
+            </div>
+
+            <div class="form-group clearfix">
+                <label class="control-label" style="float: left; padding-right: 5px;">
+                    <?= $model->attributeLabels()['email'] ?> :
+                </label>
+                <span><?= ($model->email) ? $model->email : Yii::t('yii', '(not set)') ?></span>
+            </div>
+
+            <div class="form-group clearfix">
+                <label class="control-label" style="float: left; padding-right: 5px;">
+                    <?= $model->attributeLabels()['user_ip'] ?> :
+                </label>
+                <span><?= $model->user_ip ?></span>
+            </div>
+
+            <div class="form-group clearfix">
+                <label class="control-label" style="float: left; padding-right: 5px;">
+                    <?= $model->attributeLabels()['model'] ?> :
+                </label>
+                <span><?= $model->model ?></span>
+            </div>
+
+            <div class="form-group clearfix">
+                <label class="control-label" style="float: left; padding-right: 5px;">
+                    <?= $model->attributeLabels()['parent_id'] ?> :
+                </label>
+                <span><?= $model->parent_id ?></span>
+            </div>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-12">
 
                     <?= $form->field($model, 'status')->dropDownList(Comment::getStatusList()) ?>
 
@@ -32,84 +68,20 @@ use artsoft\widgets\ActiveForm;
 
             </div>
         </div>
-
-        <div class="col-md-4">
-
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?=  $model->attributeLabels()['id'] ?>: </label>
-                            <span><?=  $model->id ?></span>
-                        </div>
-                        
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['created_at'] ?> :
-                            </label>
-                            <span><?= $model->createdDate . ' ' . $model->createdTime ?></span>
-                        </div>
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['updated_at'] ?> :
-                            </label>
-                            <span><?= $model->updatedDate . ' ' . $model->updatedTime ?></span>
-                        </div>
-                       
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['username'] ?> :
-                            </label>
-                            <span><?= $model->author ?></span>
-                        </div>
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['email'] ?> :
-                            </label>
-                            <span><?= ($model->email) ? $model->email : Yii::t('yii', '(not set)') ?></span>
-                        </div>
-                        
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['user_ip'] ?> :
-                            </label>
-                            <span><?= $model->user_ip ?></span>
-                        </div>
-                        
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['model'] ?> :
-                            </label>
-                            <span><?= $model->model ?></span>
-                        </div>
-
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['parent_id'] ?> :
-                            </label>
-                            <span><?= $model->parent_id ?></span>
-                        </div>
-
-                        <div class="form-group">
-                            <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
-
-                            <?= Html::a(Yii::t('art', 'Delete'), ['/comment/default/delete', 'id' => $model->id], [
-                                'class' => 'btn btn-danger',
-                                'data' => [
-                                    'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                    'method' => 'post',
-                                ],
-                            ])
-                            ?>
-                        </div>
-                    </div>
-                </div>
+        <div class="panel-footer">
+            <div class="form-group">
+                <?= Html::a(Yii::t('art', 'Go to list'), ['/comment/default/index'], ['class' => 'btn btn-default']) ?>
+                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
+                <?= Html::a(Yii::t('art', 'Delete'), ['/comment/default/delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
+                ])
+                ?>
             </div>
-
+            <?= \artsoft\widgets\InfoModel::widget(['model' => $model]); ?>
         </div>
     </div>
 
